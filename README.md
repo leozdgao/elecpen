a89203c24ba5a991f7748494ef60dfbf3f52fa73
leozdgao <gzdleo0816@gmail.com>
2015-06-23 13:54:26 +0800

    first release

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# logger
+# Logger
+
 A simple logger
+
+### Options
+
+|Option|Description|
+|------|-----------|
+|infoFile|File name for logging info and verbose|
+|errFile|File name for logging error and warning|
+|logToConsole|Write to console too|
+|timestamp|Add timestamp for an entry|
+|append|If file exists, append new entries to it instead of truncating|
+
+### Methods
+
+- logger.verbose
+- logger.info
+- logger.warning
+- logger.error
+
+### How to use?
+
+```
+var opts = {
+  infoFile: 'info.log',  // record info and verbose
+  errFile: 'err.log', // record error and warning
+  logToConsole: true,
+  timestamp: true, // default to true
+  append: true // default to true
+};
+var fs = requrie('http');
+var logger = require('logger').createLogger(opts);
+
+http.createServer(function(req, res) {
+  logger.info('Recieve a request. Path: %s', req.path);
+  res.end('Hello world.');
+})
+.listen(4000, function() {
+  logger.info('Server listening...');
+});
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var chalk = require('chalk');
+var util = require('util');
+
+/**
+ * Construct a logger, it can record the verbose, info , warning, error message and
+ * write it to the file you specified.
+ *
+ * Options:
+ *     infoFile: Path of the file for recording verbose and info message.
+ *     errFile: Path of the file for recording warning and error message.
+ *     append: Decide append or truncate the file if it exists. Default to be true.
+ *     timestamp: Output the timestamp or not. Default to be true.
+ *     logToConsole: Record to console after write log.
+ *
+ * @param {Object} opts - options
+ * @returns {Object} The logger
+ */
+exports.createLogger = function(opts) {
+    var infoFile = opts.infoFile, // stream for info and verbose
+        errFile = opts.errFile, // stream for error and warning
+        append = typeof opts.append == 'boolean' ? opts.append : true, // append to existed file or not
+        timestamp = typeof opts.append == 'boolean' ? opts.timestamp : true, // output timestamp or not
+        logToConsole = !!opts.logToConsole;
+
+    var flag = append ? 'a' : 'w',
+        infoStream = infoFile && fs.createWriteStream(infoFile, { flags: flag }),
+        errStream = errFile && fs.createWriteStream(errFile, { flags: flag }),
+        logger = {};
+
+    logger.verbose = function() {
+        infoStream && log(infoStream, '[Verbose]', chalk.gray).apply(this, arguments);
+    };
+    logger.info = function() {
+        infoStream && log(infoStream, '[Info]').apply(this, arguments);
+    };
+    logger.warning = function() {
+        errStream && log(errStream, '[Warning]', chalk.yellow).apply(this, arguments);
+    };
+    logger.error = function() {
+        errStream && log(errStream, '[Error]', chalk.red).apply(this, arguments);
+    };
+
+    function log(stream, prefix, decorator) {
+        var msg = prefix + ' ' + (timestamp ? getCurrentDateString(): '') + ' ';
+        return function () {
+            var args = Array.prototype.slice.call(arguments),
+                msg = util.format.apply(this, [].concat(msg, args));
+
+            stream.write(msg + '\n');
+
+            if(logToConsole) {
+              console.log(typeof decorator == 'function' ? decorator(msg) : msg);
+            }
+        };
+    }
+
+    function getCurrentDateString() {
+        return Date();
+    }
+
+    return logger;
+}
